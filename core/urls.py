0e437eea0a156751b1da7b8a8b2e0fc5641e1cfb
steveGau <95110981+steveGau@users.noreply.github.com>
2023-02-09 22:09:58 -0500

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,17 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace="blog"))
 ]
+
+sqlite_database_ID=0
+if sqlite_database_ID==1:
+    urlpatterns += staticfiles_urlpatterns()
+
 # path('home/', TemplateView.as_view(template_name="home.html")),
 
 if settings.DEBUG:
