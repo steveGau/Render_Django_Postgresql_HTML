a52ea2f417b75960328008a2b91f5e682074d29a
steveGau <95110981+steveGau@users.noreply.github.com>
2023-02-11 21:54:36 -0500

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=155)
     content = models.TextField() 
     slug = models.SlugField(max_length=255)
-    image = models.ImageField(upload_to="images/", default="images/default.png")
+    # image = models.ImageField(upload_to="images/", default="images/default.png")
 
     def get_absolute_url(self):
         return reverse("blog:single", args=[self.slug])
